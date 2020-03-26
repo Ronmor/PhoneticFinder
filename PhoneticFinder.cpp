0bbfa6016314a4b6904fcd118a15443948506a16
Ron Mor <ronmor35@gmail.com>
2020-03-26 17:50:26 +0200

    Add files via upload

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -9,6 +9,7 @@ using namespace std;
 namespace phonetic{
 
 int someBodySucksAtHisJob(char t, char w){
+
     t = tolower(t);
     w = tolower(w);
     if((t=='w'&& w=='v')||(t=='v' && w=='w'))
@@ -37,7 +38,7 @@ int someBodySucksAtHisJob(char t, char w){
         return 1;
     if((t=='i'&& w=='y')||(t=='y' && w=='i'))
         return 1;
-        
+      
     return 0;
 }
 
@@ -76,8 +77,10 @@ string find(string text ,string currentword){
             return eureka;
             }
         }
+        throw string("There is no such word in this message");
+        break;
     }
-    throw string("There is no such word in this message");
+    
     return "----The End----";
     
 }
